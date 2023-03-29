5c4a39388ac0db22aae1d55fac0e834cc9d4e5ca
Animus <Animus@ANIMUS-PC>
2023-03-29 08:22:03 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace AreaDeUmCirculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, area, pi;
+            pi = 3.14159;
+
+            Console.WriteLine("Digite o valor do raio: ");
+            raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            area = Math.Pow(raio, 2) * pi;
+            Console.WriteLine("Area corresponde a : {0}", area.ToString("F4",CultureInfo.InvariantCulture));
+        }
+    }
+}
